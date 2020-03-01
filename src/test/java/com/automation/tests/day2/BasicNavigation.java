d46a3a9fc9d7f997e2de6bd35bd84e20734b0ccc
sengin <enginsema@gmail.com>
2020-03-01 13:33:40 -0700

    Create basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -21,6 +21,8 @@ public class BasicNavigation {
         //in selenium, everything starts from WebDriver interface'
         //ChromeDriver extends RemoteWebDriver --> implements WebDriver
         driver.get("http://google.com"); // to open a website
+        driver.manage().window().maximize();// to maximize browser
+      //  driver.manage().window().fullscreen();
         Thread.sleep(3000); // for demo, wait 3 seconds
         // method return page title
         // you can also see it as tab name, in the browser
@@ -34,6 +36,26 @@ public class BasicNavigation {
         }else {
             System.out.println("Test Failed");
         }
+
+        // go to another website within the same window
+        driver.navigate().to("http://amazon.com");
+
+        if (driver.getTitle().toLowerCase().contains("amazon")){
+            System.out.println("Test Passed!");
+        }else {
+            System.out.println("Test Failed");
+        }
+
+        // comeback to google
+        driver.navigate().back();
+
+        // checking if page title is equal to Google
+        // .getTitle() - returns page title
+        verifyEquals(driver.getTitle(), "Google");
+        // move forward in the browser history
+        driver.navigate().forward();
+        System.out.println("Title : " + driver.getTitle());
+        // driver.getTitle() - returns page title of the the page that is currently opened
 
         // must be at the end
         driver.close(); // to close browser
@@ -53,5 +75,21 @@ public class BasicNavigation {
             <version>1.7.30</version>
         </dependency>
          */
+    }
+
+    /**
+     * Check if to strings are same. If print TEST PASSED! message.
+     * Otherwise, print TEST FAILED message
+     * @param arg1
+     * @param arg2
+     */
+
+    public static void verifyEquals(String arg1, String arg2){
+
+        if (arg1.equals(arg2)){
+            System.out.println("Test Passed!");
+        }else {
+            System.out.println("Test Failed");
+        }
     }
 }
